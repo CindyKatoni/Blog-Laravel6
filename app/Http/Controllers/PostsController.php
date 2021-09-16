13fdc4981bf19703f2fd628cb0b4e97fdc0fc8d9
CindyKatoni <katmuema@gmail.com>
2021-09-16 09:45:20 +0300

    Using PostsController to route posts array

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    //Create method show
+    public function show($post)
+    {
+        $posts = [
+                     'my_first_post' => 'This is the first post',
+                     'my_second_post' => 'This is the second post',
+                 ];
+            
+                 if(! array_key_exists ($post, $posts)){
+                     abort(404, 'Sorry the page doesnt exist');
+                 }
+                    
+                 return view('post', [
+                     'post' => $posts[$post]]);
+    }
 }
