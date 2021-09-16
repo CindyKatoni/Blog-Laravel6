35a74aec973355babc7486006fbcc24583915cd4
CindyKatoni <katmuema@gmail.com>
2021-09-16 14:27:07 +0300

    Create Post model,use eloquent to perform sql queries

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     //Create method show
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        // $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->first();
         // dd($post);
 
+        if (! $post){
+            abort(404);
+        }
                     
         return view('post', [
             'post' => $post]);
