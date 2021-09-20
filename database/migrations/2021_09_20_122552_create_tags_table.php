ff2a6c02b0bc4157322b463e8de022eaf5706445
CindyKatoni <katmuema@gmail.com>
2021-09-20 16:36:18 +0300

    Create pivot table to link articles and tags table. Episode 31.

--- database/migrations/2021_09_20_122552_create_tags_table.php
+++ database/migrations/2021_09_20_122552_create_tags_table.php
@@ -15,7 +15,21 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->timestamps();
+        });
+
+        //Pivot table linking the two tables(article and user.) convenction for naming article_tag
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('article_id')->unsigned()->index()->nullable();
+            $table->bigInteger('tag_id')->unsigned()->index()->nullable();
+            $table->timestamps();
+
+            $table->unique(['article_id', 'tag_id']); //Ensures no duplication
+
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
 
