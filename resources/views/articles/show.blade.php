a1b20eab2554d6f5371da25dc21be2e945b199dc
CindyKatoni <katmuema@gmail.com>
2021-09-21 11:46:56 +0300

    Display tags in show view. Episode 32

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,5 +3,12 @@
         <h2>{{ $article->title }}</h2>
         <!-- <p>{{ $article->body }}</p> -->
         {!! $article->body !!}
+
+        <p>
+        @foreach ($tags as $tag)
+            <a href="#">{{ $tag->name }}</a>
+        #endforeach    
+        </p>
+    
     </div>
 </div>
