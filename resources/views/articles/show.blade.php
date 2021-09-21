00292783ac6ea683a5d717c74915556b65a529b2
CindyKatoni <katmuema@gmail.com>
2021-09-21 10:31:17 +0300

    Edited the show view.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="title">
         <h2>{{ $article->title }}</h2>
-        <p>{{ $article->body }}</p>
-
+        <!-- <p>{{ $article->body }}</p> -->
+        {!! $article->body !!}
     </div>
 </div>
