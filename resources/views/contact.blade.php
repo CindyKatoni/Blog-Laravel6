39b5538fb9a499f36a2931fbf1ca1ecde2bbf68e
CindyKatoni <katmuema@gmail.com>
2021-09-22 17:53:45 +0300

    Added error object to throw error if invalid email is entered. Used flash message to display message after submitting email. Episode 42

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,7 +9,9 @@
     <script async defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </head>
 <body>
-<form class="row g-3">
+
+<div class="container">
+<form method="POST" action="/contact" class="row g-3">
     @csrf
 
   <div class="col-12">
@@ -22,5 +24,43 @@
     <button type="submit" class="btn btn-primary">SUBMIT</button>
   </div>
 </form>
+
+</div>
+
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html class="h-full">
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+    <title>Mail Lesson</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet">
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-full">
+    <form method="POST" action="/contact"
+          class="bg-white p-6 rounded shadow-md"
+          style="width: 300px">
+        @csrf
+        <div class="mb-5">
+            <label for="email"
+                   class="block text-xs uppercase font-semibold mb-1"
+            >Email Address</label>
+            <input id="email" type="text"  name="email"
+                   class="border px-2 py-1 text-sm w-full">
+
+            @error('email')       
+                   <div class="text-red-500 text-xs">{{ $message }}</div>
+            @enderror       
+        </div>
+        <button type="submit" 
+                class="bg-blue-500 py-2 text-white rounded-full text-sm w-full"
+        >Email Me</button>
+        
+        @if (session('message'))
+          <div>{{ session('message') }}</div>
+        @endif  
+    </form>
 </body>
 </html>
