315dd3bcbc361c7df7e37d0227e9876df4ed44ea
CindyKatoni <katmuema@gmail.com>
2021-09-22 15:59:48 +0300

    Import View,File,Request Facades and use file facade to get indexx file. Episode 40

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Request;
+// use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     //ddd(resolve('App\Example'), resolve('App\Example'));
+    public function home()
+    {
+        return File::get(public_path('index.php'));
+    }
+
 }
