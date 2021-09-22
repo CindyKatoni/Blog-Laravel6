3b45720603d24e64a99a7e2161648aa7bf09b924
CindyKatoni <katmuema@gmail.com>
2021-09-22 20:46:10 +0300

    Create new instance of ContaactMe class. ep44

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Support\Facades\Mail;
 // use Illuminate\Http\Request;
 
@@ -17,10 +18,8 @@ class ContactController extends Controller
     {
         request()->validate(['email' => 'required|email']);
 
-        Mail::raw('It Works!', function($message) {
-            $message->to(request('email'))
-                ->subject('Hello There');
-        });
+        Mail::to(request('email'))
+            ->send(new ContactMe('shirts'));
 
         return redirect('/contact')
             ->with('message', 'Email Sent!');
