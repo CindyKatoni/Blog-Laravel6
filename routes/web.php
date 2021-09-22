aa754be331b682a2ab4b33700f5d6e166c030e87
CindyKatoni <katmuema@gmail.com>
2021-09-22 15:49:37 +0300

    Removed unnecessary comments

--- routes/web.php
+++ routes/web.php
@@ -2,45 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     // return view('welcome');
-
-  
-// });
-
-// Route::get('/posts/{post}', function($post){
-//     $posts = [
-//         'my_first_post' => 'This is the first post',
-//         'my_second_post' => 'This is the second post',
-//     ];
-
-//     if(! array_key_exists ($post, $posts)){
-//         abort(404, 'Sorry the page doesnt exist');
-//     }
-        
-//     return view('post', [
-//         'post' => $posts[$post]]);
-// });
-
-// //Create service container
-// app()->bind('example', function() {
-//     return new \App\Example();
-// });
-
-
-// Route::get('/', 'PagesController@home');
-
 
 Route::get('/', function(){
     return view('welcome');
@@ -56,17 +17,6 @@ Route::get('/posts/{post}', 'PostsController@show');
 Route::get('/contact', function(){
     return view('contact');
 });
-
-
-
-// Route::get('/article', function () {
-//     $articles = App\Article::take(4)->latest()->get();
-
-//     return $articles;
-
-//     return view('article');
-// });
-
 
 Route::get('/articles', 'ArticlesController@index');
 Route::post('/articles', 'ArticlesController@store');
