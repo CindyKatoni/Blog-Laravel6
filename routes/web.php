cc77d11d5107f6c5f095fa676210f55f155b123c
CindyKatoni <katmuema@gmail.com>
2021-09-24 10:53:18 +0300

    Route for all conversations, one conversation, best reply. Episode 50.

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,10 @@ Route::post('/contact', 'ContactController@store');
 Route::get('/payments/create', 'PaymentController@create')->middleware('auth');
 Route::post('/payments', 'PaymentController@store')->middleware('auth');
 Route::get('notifications', 'UserNotificationController@show')->middleware('auth');
-
-
+Route::get('conversations', 'ConversationController@index');
+Route::get('conversations/{conversation}', 'ConversationController@show');
+// Route::get('conversations/{conversation}', 'ConversationController@show')->middleware('can:view,conversation');
+Route::post('/best-replies/{reply}', 'ConversationBestReplyController@store');
 
 
  //Using HTTP Verbs for routing
